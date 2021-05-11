c87d288bf0b8f01846c67dfb7f03d6dc45112d2b
Filip Jirsák <filip@jirsak.org>
2021-05-11 23:04:03 +0200

    Výsledné řešení.

--- src/main/java/cz/czechitas/java2webapps/lekce7/controller/CalendarController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce7/controller/CalendarController.java
@@ -1,15 +1,61 @@
 package cz.czechitas.java2webapps.lekce7.controller;
 
+import cz.czechitas.java2webapps.lekce7.service.CalendarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Filip Jirsák
  */
 @Controller
 public class CalendarController {
-  @GetMapping("/")
-  public String index() {
-    return "index";
+  private final CalendarService service;
+
+  @Autowired
+  public CalendarController(CalendarService service) {
+    this.service = service;
   }
+
+  @GetMapping("/")
+  public ModelAndView index() {
+    return new ModelAndView("index")
+            .addObject("currentYear", service.getCurrentYear());
+  }
+
+  @GetMapping("/vypocet")
+  public String form(int rok, String typ) {
+    return String.format("redirect:/%s/%d", typ, rok);
+  }
+
+  @GetMapping("/easter/{year}")
+  public ModelAndView easter(@PathVariable int year) {
+    return new ModelAndView("easter")
+            .addObject("year", year)
+            .addObject("calendar", service.easter(year));
+  }
+
+  @GetMapping("/pentecost/{year}")
+  public ModelAndView pentecost(@PathVariable int year) {
+    return new ModelAndView("pentecost")
+            .addObject("year", year)
+            .addObject("calendar", service.pentecost(year));
+  }
+
+  @GetMapping("/christmas/{year}")
+  public ModelAndView christmas(@PathVariable int year) {
+    return new ModelAndView("christmas")
+            .addObject("year", year)
+            .addObject("calendar", service.christmas(year));
+  }
+
+  @GetMapping("/sylvester/{year}")
+  public ModelAndView sylvester(@PathVariable int year) {
+    return new ModelAndView("sylvester")
+            .addObject("year", year)
+            .addObject("calendar", service.sylvester(year));
+  }
+
 }
